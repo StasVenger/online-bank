6045571378b5d647772133aebdf8eabbf9a20368
Stanislav Venger <SVenger@actsoft.com>
2022-01-22 19:04:21 +0600

    Created basic vue app

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <the-nav-bar></the-nav-bar>
+  <div class="container with-nav">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavBar from "@/components/TheNavBar";
+export default {
+  components: {TheNavBar}
+}
+</script>
+
+<style scoped>
+
+</style>
